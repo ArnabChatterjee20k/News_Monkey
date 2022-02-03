ad2856056f1be22d9507f309da85e77a2dd36c01
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-02-03 19:29:22 +0530

    revised constructor and added static image

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import News_Item from './News_Item';
 
 export class News extends Component {
+  
   render() {
     return (
       <div className='container m-3 p-2'>
         <h2>News Monkey - Top Headlines</h2>
         <div className="row">
-          
+
           <div className="col-md-4">
-            <News_Item title="arnab" description="news" />
+            <News_Item title="arnab" description="news" image_url="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" />
           </div>
 
           <div className="col-md-4">
--- src/Components/News_Item.jsx
+++ src/Components/News_Item.jsx
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 
 export class News_Item extends Component {
+    constructor() {
+        super(); // if we make a constructor then we have to call surely the super in react otherwise we will get error.
+        console.log("constructor") // all the items will call constructor. Now three items are their so 3 times this function will run
+        // constructor will run everytime when object are made of this class.
+    }
     render() {
-        let { title, description } = this.props; // using object destructuring
+        let { title, description, image_url } = this.props; // using object destructuring
         return (
-                <div className="card m-3" style={{width:"18rem"}}>
-                    <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">{title}</h5>
-                            <p className="card-text">{description}</p>
-                            <a href="/" className="btn btn-primary">Go somewhere</a>
-                        </div>
+            <div className="card m-3" style={{ width: "18rem" }}>
+                <img src={image_url} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <a href="/" className="btn btn-sm btn-primary">Read More...</a>
                 </div>
+            </div>
 
         );
     }
