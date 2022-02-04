d3c3d2e56841717ae4bec9b409a3b00017fdc88f
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-02-04 15:23:22 +0530

    added next and previous button. Added end of pages logic using pages query string in the api endpoint

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -4,31 +4,62 @@ import News_Item from './News_Item';
 export class News extends Component {
   constructor() {
     super();
-    
+
     this.state = {
       articles: [],
-      loading: false
+      loading: false,
+      page: 1 // page number by default 1
     }
   }
-
-  async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=5e7a8ec386ee44b292ebeb2437cba363";
-    let data = await fetch(url); 
-    let parsed_data = await data.json() ; 
-    
-    this.setState({articles : parsed_data.articles})
+  fetch_change_state = async () => {
+    // this method will be used to change the state after fetching data
   }
+
+  async componentDidMount() {
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=5e7a8ec386ee44b292ebeb2437cba363&pageSize=20";
+    let data = await fetch(url);
+    let parsed_data = await data.json();
+    this.setState({ articles: parsed_data.articles, total_results: parsed_data.totalResults })
+  }
+
+  handle_prev_click = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=5e7a8ec386ee44b292ebeb2437cba363&page=${this.state.page - 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsed_data = await data.json();
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsed_data.articles
+    })
+  }
+
+  handle_next_click = async () => {
+    if (!(this.state.page + 1 > Math.ceil(this.state.total_results / 20))) { // checking ending of the page
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=5e7a8ec386ee44b292ebeb2437cba363&page=${this.state.page + 1}&pageSize=20`;
+      let data = await fetch(url);
+      let parsed_data = await data.json();
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsed_data.articles
+      })
+    }
+
+    // console.log(this.state.page) it is giving previous result due to asynchronous nature of js. While state is getting change console is showing the previous value.
+  }
+
   render() {
     return (
       <div className='container p-3'>
         <h2>News Monkey - Top Headlines</h2>
         <div className="row">
-          {this.state.articles.map(({title , description , urlToImage ,url}) => {
+          {this.state.articles.map(({ title, description, urlToImage, url }) => {
             return <div className="col-md-4" key={url} >
-              <News_Item title={title?`${title.slice(0,30)}....`:null} description={description?`${description.slice(0,60)}....`:null} image_url={urlToImage?urlToImage:"https://images.moneycontrol.com/static-mcnews/2020/01/BSE_Sensex_Stocks_market-770x433.png"} news_url={url} /> {/* handled the null condition  using ternary operator*/}
+              <News_Item title={title ? `${title.slice(0, 30)}....` : null} description={description ? `${description.slice(0, 60)}....` : null} image_url={urlToImage ? urlToImage : "https://images.moneycontrol.com/static-mcnews/2020/01/BSE_Sensex_Stocks_market-770x433.png"} news_url={url} /> {/* handled the null condition  using ternary operator*/}
             </div>
           })}
-
+        </div>
+        <div className="container m-3 p-3">
+          <button disabled={this.state.page <= 1} type="button" className="btn btn-warning float-start" onClick={this.handle_prev_click}>&larr; Previous</button>
+          <button type="button" className="btn btn-warning float-end" onClick={this.handle_next_click}>Next &rarr;</button>
         </div>
       </div>
     );
