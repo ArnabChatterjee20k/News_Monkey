6858b66e8be3cea5830d4fd3d15ca572349ca020
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-02-05 13:57:32 +0530

    comments deleted. See them from previous commits

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -19,12 +19,12 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: true,
-      page: 1 // page number by default 1
+      page: 1 
     }
   }
 
   fetch_page_content = async (state_changing_obj) => {
-    let { page = this.state.page } = state_changing_obj; // setting default value of page if page not passed
+    let { page = this.state.page } = state_changing_obj;
     let url = `https://newsapi.org/v2/${this.props.category}?country=${this.props.country}&apiKey=5e7a8ec386ee44b292ebeb2437cba363&page=${page}&pageSize=${this.props.pageSize}`;
     this.setState({loading:true})
     let data = await fetch(url);
@@ -42,11 +42,10 @@ export class News extends Component {
   }
 
   handle_next_click = async () => {
-    if (!(this.state.page + 1 > Math.ceil(this.state.total_results / this.props.pageSize))) { // checking ending of the page
+    if (!(this.state.page + 1 > Math.ceil(this.state.total_results / this.props.pageSize))) {
       let passing_obj = { page: this.state.page + 1 }
       this.fetch_page_content(passing_obj)
     }
-    // console.log(this.state.page) it is giving previous result due to asynchronous nature of js. While state is getting change console is showing the previous value.
   }
 
   render() {
@@ -56,10 +55,10 @@ export class News extends Component {
         <h2>News Monkey - Top Headlines</h2>
         <div className="row">
 
-          {this.state.loading && <div className="text-center m-3 p-3"> <Spinner_loading /> </div>} {/* if loading is false then due to short circuting this statement will not run  */}
-          {!this.state.loading && this.state.articles.map(({ title, description, urlToImage, url }) => { // if loading is true then this statement will not run as not true is false. 
+          {this.state.loading && <div className="text-center m-3 p-3"> <Spinner_loading /> </div>}
+          {!this.state.loading && this.state.articles.map(({ title, description, urlToImage, url }) => {
             return <div className="col-md-4" key={url} >
-              <News_Item title={title ? `${title.slice(0, 30)}....` : null} description={description ? `${description.slice(0, 60)}....` : null} image_url={urlToImage ? urlToImage : "https://images.moneycontrol.com/static-mcnews/this.props.pageSizethis.props.pageSize/01/BSE_Sensex_Stocks_market-770x433.png"} news_url={url} /> {/* handled the null condition  using ternary operator*/}
+              <News_Item title={title ? `${title.slice(0, 30)}....` : null} description={description ? `${description.slice(0, 60)}....` : null} image_url={urlToImage ? urlToImage : "https://images.moneycontrol.com/static-mcnews/this.props.pageSizethis.props.pageSize/01/BSE_Sensex_Stocks_market-770x433.png"} news_url={url} />
             </div>
           })}
         </div>
