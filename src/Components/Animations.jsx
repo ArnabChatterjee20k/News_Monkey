a65b7d312d85a90d0787ba62f8d3633f8a6a1278
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-02-07 21:56:46 +0530

    Animation classbased to function based conversion

--- src/Components/Animations.jsx
+++ src/Components/Animations.jsx
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import spinner from "../Assets/Spinner.gif"
 import loading from "../Assets/Loading.gif"
 
-export class Spinner_heading extends Component {
-    render() {
+export function Spinner_heading () {
         return <div className='container'>
             <img src={spinner} alt="loading" />
         </div>;
-    }
 }
-export class Spinner_loading extends Component {
-    render() {
+export function Spinner_loading () {
         return <div className='container'>
             <img src={loading} alt="loading" />
         </div>;
-    }
 }
